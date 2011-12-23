dd5d39ef0635d9aebac5041c9a0f1986b4ac5728
irp <numa90@hotmail.it>
2011-12-23 19:58:58 +0100

    Update src/dbg.cpp

--- src/dbg.cpp
+++ src/dbg.cpp
@@ -7,16 +7,16 @@
 
 CONTEXT Context;
 STACKFRAME StackFrame;
-PSYMBOL_INFOW pSymbol;
 DEBUG_EVENT dbgEvent = { 0 };
+MEMORY_BASIC_INFORMATION lpBuffer;
 
 BOOL NTAPI SetStackFrame
-    (__in      HANDLE hProcess,
-     __in      HANDLE hThread,
-     __inout   STACKFRAME stackFrame,
-     __inout   PCONTEXT context,
-     __in_opt  PFUNCTION_TABLE_ACCESS_ROUTINE FunctionTableAccessRoutine,
-     __in_opt  PGET_MODULE_BASE_ROUTINE GetModuleBaseRoutine) {
+	(__in      HANDLE hProcess,
+         __in      HANDLE hThread,
+         __inout   STACKFRAME stackFrame,
+         __inout   PCONTEXT context,
+         __in_opt  PFUNCTION_TABLE_ACCESS_ROUTINE FunctionTableAccessRoutine,
+         __in_opt  PGET_MODULE_BASE_ROUTINE GetModuleBaseRoutine) {
 
 	         ZeroMemory(&stackFrame, sizeof(stackFrame));
 		 if (context != NULL) {
@@ -26,7 +26,7 @@ BOOL NTAPI SetStackFrame
 	         stackFrame.AddrStack.Offset = context->Esp;
 	         stackFrame.AddrStack.Mode = AddrModeFlat;
 	         stackFrame.AddrFrame.Offset = context->Ebp;
-		 stackFrame.AddrFrame.Mode = AddrModeFlat; }
+	         stackFrame.AddrFrame.Mode = AddrModeFlat; }
 
 		 //Note: MachineType IMAGE_FILE_MACHINE_I386
 		 //so if I've no context I've to initialize the 
@@ -50,88 +50,112 @@ BOOL NTAPI SetStackFrame
 		 if (hProcess != INVALID_HANDLE_VALUE) {
 			if (StackWalk(IMAGE_FILE_MACHINE_I386, hProcess, hThread, 
 				      &StackFrame, context, 0, FunctionTableAccessRoutine, 
-				      GetModuleBaseRoutine, 0)) {
-					 return TRUE; }
+				      GetModuleBaseRoutine, 0)) {          return TRUE;               }
+				      
 			else return FALSE; 
 		 }
 
 		 else return FALSE;
 }
 
-PSYMBOL_INFOW NTAPI SetSymbolInformationForAddress
-	(__in HANDLE hProcess,
-	 __in DWORD Address) {
-
-		char buf[sizeof(PSYMBOL_INFOW) + MAX_SYM_NAME * sizeof(TCHAR)];
-		ZeroMemory(buf, sizeof(buf));
-                PSYMBOL_INFOW Symbol = (PSYMBOL_INFOW)buf;
-
-                Symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
-                Symbol->MaxNameLen = MAX_SYM_NAME;
-
-		if (SymFromAddrW(hProcess, Address, 0, Symbol)) {
-			return Symbol; }
-		else return NULL;
-}
-
 DWORD NTAPI OnAccessViolationException
 	(__in HANDLE hThread,
 	 __in HANDLE hProcess,
 	 __in LPDEBUG_EVENT DbgEvent) {
-		 	
-		Context.ContextFlags = CONTEXT_INTEGER;
-		if (GetThreadContext(hThread, &Context)) {
-				printf("Edi %08x Esi %08x Ebx %08x ", Context.Edi, Context.Esi, Context.Ebx);
-				printf("Edx %08x Ecx %08x Eax %08x\n", Context.Edx, Context.Ecx, Context.Eax); }
-		Context.ContextFlags = CONTEXT_CONTROL;
-		if (GetThreadContext(hThread, &Context)) {
-				printf("Ebp %08x Eip %08x Esp %08x\n", Context.Ebp, Context.Eip, Context.Esp); }
-		while (SetStackFrame(hProcess, hThread, StackFrame, &Context, SymFunctionTableAccess, SymGetModuleBase)) {
-		     pSymbol = SetSymbolInformationForAddress(hProcess, StackFrame.AddrPC.Offset);
-			 printf("Flags: %08x\n", &pSymbol->Flags);
-			 } 	 
 
-		return DBG_EXCEPTION_NOT_HANDLED;
+        PSYMBOL_INFOW pSymbol;
+	char buf[sizeof(PSYMBOL_INFOW) + MAX_SYM_NAME * sizeof(TCHAR)];
+	ZeroMemory(buf, sizeof(buf));
+        pSymbol = (PSYMBOL_INFOW)buf;
+
+        pSymbol->SizeOfStruct = sizeof(SYMBOL_INFO);
+        pSymbol->MaxNameLen = MAX_SYM_NAME;
+
+        if (SymFromAddrW(hProcess, StackFrame.AddrPC.Offset, 0, pSymbol)) {
+
+		 Context.ContextFlags = CONTEXT_INTEGER;
+		 if (GetThreadContext(hThread, &Context)) {
+		       printf("Edi %08x Esi %08x Ebx %08x ", Context.Edi, Context.Esi, Context.Ebx);
+		       printf("Edx %08x Ecx %08x Eax %08x\n", Context.Edx, Context.Ecx, Context.Eax); }
+		 Context.ContextFlags = CONTEXT_CONTROL;
+		 if (GetThreadContext(hThread, &Context)) {
+		      printf("Ebp %08x Eip %08x Esp %08x\n", Context.Ebp, Context.Eip, Context.Esp); }
+		      while (SetStackFrame(hProcess, hThread, StackFrame, &Context, 
+		                           SymFunctionTableAccess, SymGetModuleBase)) {
+                         printf("nameLen: %d\n", pSymbol->NameLen);
+			 printf("Flags: %08x\n", pSymbol->Flags);
+			 _tprintf(_T("Name: %s\n"), pSymbol->Name);
+			 
+		       }
+		 }
+		 else printf("ERROR: SymFromAddrW failed with code: %d\n", GetLastError());
+
+	return DBG_EXCEPTION_NOT_HANDLED;
 }
 
 BOOL NTAPI SetDbgEvents
 	(__in LPDEBUG_EVENT DbgEvent) {
 
-		HANDLE hProcess, hThread;
-		DWORD dwContinueStatus = DBG_CONTINUE;
+	HANDLE hProcess, hThread;
+	DWORD dwContinueStatus = DBG_CONTINUE, oldProtection;
+	PVOID pImageName, pBaseOfDll;
+	WCHAR  ImageName;
+	ULONG_PTR sizeRead, szRead;
+	BOOL value;
 
-		for (;;) {
-			if (WaitForDebugEvent(DbgEvent, INFINITE)) {
-				hThread = OpenThread(THREAD_SET_CONTEXT | THREAD_GET_CONTEXT,
-						     FALSE, DbgEvent->dwThreadId);
-				hProcess = OpenProcess(PROCESS_ALL_ACCESS,
-						       FALSE, DbgEvent->dwThreadId);				
-		                SetThreadContext(hThread, &Context);
-				SymInitialize(hProcess, NULL, FALSE);
-				switch (DbgEvent->dwDebugEventCode) {
-				case EXCEPTION_DEBUG_EVENT:
-					switch (DbgEvent->u.Exception.ExceptionRecord.ExceptionCode) {
-					case EXCEPTION_ACCESS_VIOLATION:
-				             printf("EXCEPTION_ACCESS_VIOLATION\n");			
-				             SuspendThread(hThread);
-					     if (DbgEvent->u.Exception.dwFirstChance == 1) printf("!!First chance!!\n");
-					     dwContinueStatus = OnAccessViolationException(hThread, hProcess, DbgEvent);
-					     ResumeThread(hThread);
-				             break;
+	for (;;) {
+		if (WaitForDebugEvent(DbgEvent, INFINITE)) {
+		    hThread = OpenThread(THREAD_SET_CONTEXT | THREAD_GET_CONTEXT,
+					 FALSE, DbgEvent->dwThreadId);
+	            hProcess = OpenProcess(PROCESS_VM_READ | PROCESS_ALL_ACCESS | PROCESS_VM_OPERATION,
+					   FALSE, DbgEvent->dwProcessId);				
+		    SetThreadContext(hThread, &Context);
+		    SymInitializeW(hProcess, NULL, FALSE);
+		    switch (DbgEvent->dwDebugEventCode) {
+		    case EXCEPTION_DEBUG_EVENT:
+			switch (DbgEvent->u.Exception.ExceptionRecord.ExceptionCode) {
+			case EXCEPTION_ACCESS_VIOLATION:
+				printf("EXCEPTION_ACCESS_VIOLATION\n");			
+				SuspendThread(hThread);
+				if (DbgEvent->u.Exception.dwFirstChance == 1) printf("!!First chance!!\n");
+				dwContinueStatus = OnAccessViolationException(hThread, hProcess, DbgEvent);
+				ResumeThread(hThread);
+				break;
 
-					default:
-					     break;
-						}
-				        break;
+			default:
+			        break;
+		          }
+				    break;
+		    case LOAD_DLL_DEBUG_EVENT:
+		    VirtualProtectEx(hProcess, DbgEvent->u.LoadDll.lpImageName, sizeof(DbgEvent->u.LoadDll.lpImageName), 
+			             PAGE_READONLY, &oldProtection);
+                    value = ReadProcessMemory(hProcess, DbgEvent->u.LoadDll.lpImageName, 
+			                      &pImageName, sizeof(DbgEvent->u.LoadDll.lpImageName), &sizeRead);
+		    if (value == FALSE)     printf("ERROR: ReadProcessMemory failed reading lpImageName: %d\n", GetLastError());
+	           
+	            if (pImageName != NULL) {
+	            	
+		         if (ReadProcessMemory(hProcess, &pImageName, &ImageName, lpBuffer.RegionSize, &szRead)) {
+				  VirtualProtectEx(hProcess, DbgEvent->u.LoadDll.lpBaseOfDll, sizeof(DbgEvent->u.LoadDll.lpBaseOfDll), 
+						   PAGE_READONLY, &oldProtection);
+				  ReadProcessMemory(hProcess, DbgEvent->u.LoadDll.lpBaseOfDll,
+				                    &pBaseOfDll, sizeof(DbgEvent->u.LoadDll.lpBaseOfDll), &sizeRead);
+		                  SymLoadModuleExW(hProcess, DbgEvent->u.LoadDll.hFile, &ImageName, NULL, 
+			                          (DWORD)DbgEvent->u.LoadDll.lpBaseOfDll, szRead, NULL, 0);
+									
+                         }
+			 else printf("ERROR: ReadProcessMemory failed reading the module name!\n");
+	            }
+		    else printf("ERROR: pImageName is NULL!\n");
 
-				default:
-					break;
-				}
-				CloseHandle(hThread);
+		    default:
+			break;
+		    }
+		    CloseHandle(hThread);
 
-			}
+		}
 			if (dwContinueStatus == DBG_EXCEPTION_NOT_HANDLED) {
-				printf("Exception not handled!\n");
+				printf("ERROR: Exception not handled!\n");
 				DebugBreakProcess(hProcess); }
 			else ContinueDebugEvent(DbgEvent->dwProcessId, DbgEvent->dwThreadId, dwContinueStatus); 
                         CloseHandle(hProcess);
@@ -141,27 +165,28 @@ BOOL NTAPI SetDbgEvents
 int __cdecl main
 	() {
 
-		PROCESS_INFORMATION procInfo;
-		STARTUPINFO startupInfo; 
+        PROCESS_INFORMATION procInfo;
+	STARTUPINFO startupInfo; 
  
-                ZeroMemory(&startupInfo, sizeof(startupInfo)); 
-                startupInfo.cb = sizeof(startupInfo); 
-                ZeroMemory(&procInfo, sizeof(procInfo));
+        ZeroMemory(&startupInfo, sizeof(startupInfo)); 
+        startupInfo.cb = sizeof(startupInfo); 
+        ZeroMemory(&procInfo, sizeof(procInfo));
 
-		printf("Welocome to dbg\n");
-		if (CreateProcessW(L"C:\\jump.exe", NULL, NULL, NULL, 
-			           FALSE, DEBUG_ONLY_THIS_PROCESS, NULL,
-				   NULL, &startupInfo, &procInfo)) {
+	printf("Welocome to dbg\n");
+	if (CreateProcessW(L"C:\\jump.exe", NULL, NULL, NULL, 
+			   FALSE, DEBUG_ONLY_THIS_PROCESS, NULL,
+			   NULL, &startupInfo, &procInfo)) {
 
-					 if (SetDbgEvents(&dbgEvent)) {
-								   
-					 }
-		}
+			if (SetDbgEvents(&dbgEvent)) {
 
-		int c = getchar();
-		return 0;
+			 }
+	}
+
+	int c = getchar();
+	return 0;
 }
 
 			              
+
 
 
